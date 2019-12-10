b4cd0ba66ef186fe1b5c5e4a6ea1055323b8a191
Gerardo Albor <gerardoalborfdz@gmail.com>
2019-12-10 11:34:21 +0100

    Added AllView, view of all equipments in grid format

--- DemoAPI/Controllers/HomeController.cs
+++ DemoAPI/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DemoAPI.Models.OeeReport;
 
 namespace DemoAPI.Controllers
 {
@@ -11,6 +12,20 @@ namespace DemoAPI.Controllers
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
+
+            return View();
+        }
+
+        public ActionResult AllEquipment()
+        {
+            ViewBag.Title = "All View";
+
+            return View(ReportBuilder.GetAllViewData());
+        }
+
+        [Route("Home/EquipmentFull/{id=int}")]
+        public ActionResult EquipmentFull(int id)
+        {
 
             return View();
         }
